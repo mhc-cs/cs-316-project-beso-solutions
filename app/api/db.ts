719482dad99db3942fcb639af76dd9924658d42a
GitxGot <rodri27b@mtholyoke.edu>
2024-04-04 14:32:21 -0400

    rough draft of db set up

--- app/api/db.ts
+++ app/api/db.ts
@@ -2,18 +2,30 @@ import mongoose from 'mongoose';
 
 // Connect to the database
 mongoose.connect(process.env.MONGODB_URI!);
-
+const sizes = ['xxs', 'xs', 's', 'm', 'l','xl']
+const sexes = ['female', 'male', 'unisex']
+const productTypes = ['shirt', 't-shirt', 'shorts', 'jeans']
+const ages = ['adult', 'kid']
+const colors = ['blue', 'red','green','orange','purple']
 // Define the schema for the data
 const ProductSchema = new mongoose.Schema({
-        size: String, //small, med, large 
+        productName: String,
+        description: String, //client description of their product
+        price: Number,
+        size:  { type: String, enum: sizes },
         category: String, //clothing, accessories, ect
-        cut: String ,//long short,
+        productType: { type: String,  enum: productTypes},//shirt, t shirt, 
+        sex:  { type: String, enum: sexes },
         material: String, //jean, fabric, ect
+        stock: Number, //how much is in stock
+        ageRange:  { type: String,  enum: ages},
+        color:  { type: String,  enum: colors},
 });
+
 
 // Create a model.  This is what provides the nice API to
 // manipulate the database.
-export const ProductModel = mongoose.model ('messages', ProductSchema);
+export const ProductModel = mongoose.model ('products', ProductSchema);
 
 // Make the model and schema available
 module.exports = ProductModel;
--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,21 @@
 import {NextResponse} from 'next/server';
 import {ProductModel} from './db';
+// import {stemmer} from 'stemmer';
 
 // Gets all language data from the DB
 export async function GET(request: Request) {
-  const res = await ProductModel.find({});
-  return NextResponse.json(res || {});
+    var req:string[]=["shirt", "female"]; 
+    const res = await ProductModel.find(
+        {productType: req[0], sex:req[1], ageRange:req[2], color:req[3], stock:{$gt:0}}
+        );
+    
+    return NextResponse.json(res || {});
 }
+
+//how do i specify the request? and params for that request
+//advice on handling sizes
+
+//images
+
+//cart: i need product id, quantinty,and size
+
