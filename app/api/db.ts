9663429132422229139e8e70a0c708422f1bfcbd
GitxGot <rodri27b@mtholyoke.edu>
2024-04-07 23:05:14 -0400

    db: schema restructure

--- app/api/db.ts
+++ app/api/db.ts
@@ -2,29 +2,57 @@ import mongoose from 'mongoose';
 
 // Connect to the database
 mongoose.connect(process.env.MONGODB_URI!);
-const sizes = ['xxs', 'xs', 's', 'm', 'l','xl']
-const sexes = ['female', 'male', 'unisex']
-const productTypes = ['shirt', 't-shirt', 'shorts', 'jeans']
-const ages = ['adult', 'kid']
-const colors = ['blue', 'red','green','orange','purple']
+const productCategories = ['shirt', 't-shirt', 'shorts', 'jeans']
+
 // Define the schema for the data
 const ProductSchema = new mongoose.Schema({
-        productName: String,
+        name: String,
         description: String, //client description of their product
-        price: Number,
-        size:  { type: String, enum: sizes },
-        category: String, //clothing, accessories, ect
-        productType: { type: String,  enum: productTypes},//shirt, t shirt, 
-        sex:  { type: String, enum: sexes },
-        material: String, //jean, fabric, ect
-        stock: Number, //how much is in stock
-        ageRange:  { type: String,  enum: ages},
-        color:  { type: String,  enum: colors},
+        category: { type: String,  enum: productCategories},//shirt, t shirt, 
+        material: String, //jean, cotton, ect
+        colors:[
+            {
+                color: String, //dark wash/ medium wash
+                sizes:[{
+                    size: String,
+                    inseams:[{
+                        image: String,
+                        inseam: Number,
+                        price: Number,
+                        stock: Number,
+                    }]
+                }]
+            }
+        ],
+        
 });
+
+// const Pants = new mongoose.Schema({
+//     category: "Pants",//shirt, t shirt, 
+//     name: String,
+//     productType:  String, //ex: ['jean', 'long', 'short', 'cargo']
+//     description: String, //client description of their product
+//     material: String, //jean, cotton, ect
+//     colors:[
+//         {
+//             color: String,
+//             sizes:[{
+//                 size: String,
+//                 inseams:[{
+//                     inseam: Number,
+//                     price: Number,
+//                     stock: Number,
+//                 }]
+//             }]
+//         }
+//     ],
+    
+// });
 
 
 // Create a model.  This is what provides the nice API to
 // manipulate the database.
+
 export const ProductModel = mongoose.model ('products', ProductSchema);
 
 // Make the model and schema available
