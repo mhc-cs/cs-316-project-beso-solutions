75d5e1f315a0c1c9a4c9d7d75659bb5598ba37df
GitxGot <rodri27b@mtholyoke.edu>
2024-04-11 10:06:20 -0400

    db make structure less redundant

--- app/api/db.ts
+++ app/api/db.ts
@@ -3,21 +3,26 @@ import { DevBundlerService } from 'next/dist/server/lib/dev-bundler-service';
 
 // Connect to the database
 mongoose.connect(process.env.MONGODB_URI!);
-const productCategories = ['shirt', 't-shirt', 'shorts', 'jeans']
+// const productCategories = ['shirt', 't-shirt', 'shorts', 'jeans']
+
+const ImageSchema = new mongoose.Schema({ 
+    img: { 
+       data: Buffer, 
+       contentType: String 
+    }
+ }
+);
 
 // Define the schema for the data
 const ProductSchema = new mongoose.Schema({
         name: String,
         description: String, //client description of their product
-        category: { type: String,  enum: productCategories},//shirt, t shirt, 
+        category: String,//shirt, t shirt, 
         material: String, //jean, cotton, ect
         colors:[
             {
                 color: String, //dark wash/ medium wash
-                image: { 
-                    data: Buffer, 
-                    contentType: String 
-                 },
+                image: [ImageSchema],
                 sizes:[{
                     size: String,
                     inseams:[{
@@ -58,13 +63,7 @@ const ProductSchema = new mongoose.Schema({
 // Create a model.  This is what provides the nice API to
 // manipulate the database.
 
-const ImageSchema = new mongoose.Schema({ 
-    img: { 
-       data: Buffer, 
-       contentType: String 
-    }
- }
-);
+
 
 export const ImageModel = mongoose.model ('images', ImageSchema);
 
