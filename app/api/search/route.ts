e4f0a41e0864f751faed38af114a60b5914d9c05
GitxGot <rodri27b@mtholyoke.edu>
2024-04-16 15:43:49 -0400

    search: add queiries for every combo of search cats

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -10,92 +10,84 @@ export async function GET(request: Request) {
     const searchParams = new URLSearchParams(url.search);
 
     if (searchParams.size == 1){
-        const results = ProductModel.find(
-            {category: searchParams.get('category')} //stock:{$gt:0}
+        const results = await ProductModel.find(
+            {category: searchParams.get('category'),"colors.sizes.inseams.stock":{$gt:4}}
             );
         
         return NextResponse.json(results || {});
-    }else if (searchParams.size ==2){
-        const results = ProductModel.find(
-            {category: searchParams.get('category'), "colors.sizes.size":searchParams.get('size') } //stock:{$gt:0}
+    }else if (searchParams.get('color')==null){  
+        if (searchParams.get('size')==null){//cat,...
+            if (searchParams.get('inseam')==null){//cat
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'),"colors.sizes.inseams.stock":{$gt:4}}
+                    );
+                
+                return NextResponse.json(results || {});
+            }else{//cat,inseam
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'), 
+                    "colors.sizes.inseams.inseam":searchParams.get('inseam'),
+                    "colors.sizes.inseams.stock":{$gt:4}}
+                );
+                return NextResponse.json(results || {});
+            }
+        }
+        else if (searchParams.get('inseam')==null){ //cat, size
+            const results = await ProductModel.find(
+                {category: searchParams.get('category'), 
+                "colors.sizes.size":searchParams.get('size'),
+                "colors.sizes.inseams.stock":{$gt:4}}
             );
-        
+            return NextResponse.json(results || {});
+        }
+        //cat, size, inseam
+        const results = await ProductModel.find(
+            {category: searchParams.get('category'), 
+            "colors.sizes.size":searchParams.get('size'),
+            "colors.sizes.inseams.inseam":searchParams.get('inseam'),
+            "colors.sizes.inseams.stock":{$gt:4}}
+        );
         return NextResponse.json(results || {});
-
+        
+    }else{//cat, color,...
+        if (searchParams.get('size')==null){
+            if (searchParams.get('inseam')==null){ //cat, color
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'), 
+                    "colors.color":searchParams.get('color'),
+                    "colors.sizes.inseams.stock":{$gt:4}}
+                );
+                return NextResponse.json(results || {});
+            }else{ //cat, color, inseam
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'), 
+                    "colors.sizes.color":searchParams.get('color'),
+                    "colors.sizes.inseams.inseam":searchParams.get('inseam'),
+                    "colors.sizes.inseams.stock":{$gt:4}}
+                );
+                return NextResponse.json(results || {});
+            }
+        }else{//cat, color, size, ..
+            if (searchParams.get('inseam')==null){//cat, color, size
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'), 
+                    "colors.color":searchParams.get('color'),
+                    "colors.sizes.size":searchParams.get('size'),
+                    "colors.sizes.inseams.stock":{$gt:4}}
+                );
+                return NextResponse.json(results || {});
+            }else{ //cat, color, size, inseam
+                const results = await ProductModel.find(
+                    {category: searchParams.get('category'), 
+                    "colors.sizes.color":searchParams.get('color'),
+                    "colors.sizes.size":searchParams.get('size'),
+                    "colors.sizes.inseams.inseam":searchParams.get('inseam'),
+                    "colors.sizes.inseams.stock":{$gt:4}}
+                );
+                return NextResponse.json(results || {});
+            }
+        }
     }
-
-    // }else if (searchParams.size ==2){
-
-    //     const results = await ProductModel.aggregate([
-    //         {$unwind : "$colors"},
-    //         {$match : {category:searchParams.get('category'), "colors.color": searchParams.get('colors')}},
-    //         {$project : {_id : 1,
-    //             name: 1,
-    //             color : "$colors.color",
-    //             image: "$colors.image",
-    //             sizes : "$colors.sizes"}}
-    //         ])
-
-    //     return NextResponse.json(results || {});
-
-    // }else if (searchParams.size ==3){
-        
-    //     const results = await ProductModel.aggregate([
-    //         {$unwind : "$colors"},
-    //         {$match : {category:searchParams.get('category'), "colors.color": searchParams.get('colors')}},
-    //         {$project : {_id : 1,
-    //             name: 1,
-    //             color : "$colors.color",
-    //             image: "$colors.image",
-    //             sizes : "$colors.sizes"}},
-    //         {$unwind : "$sizes"},
-    //         {$match : {"sizes.size":searchParams.get('size')}},
-    //         {$project : {_id : 1,
-    //             name: 1,
-    //             color : 1,
-    //             image: 1,
-    //             size : "$sizes.size",
-    //             inseams:"$sizes.inseams"}}
-    //         ])
-    //     return NextResponse.json(results || {});
-
-
-    // }else if(searchParams.size ==4){ //inseams
-        
-    //     const results = await ProductModel.aggregate([
-    //         {$unwind : "$colors"},
-    //         {$match : {category:searchParams.get('category'), "colors.color": searchParams.get('colors')}},
-    //         {$project : {_id : 1,
-    //             name: 1,
-    //             color : "$colors.color",
-    //             image: "$colors.image",
-    //             sizes : "$colors.sizes"}},
-    //         {$unwind : "$sizes"},
-    //         {$match : {"sizes.size":searchParams.get('size')}},
-    //         {$project : {_id : 1,
-    //             name: 1,
-    //             color : 1,
-    //             image: 1,
-    //             size : "$sizes.size",
-    //             inseams:"$sizes.inseams"}},
-    //         {$unwind : "$inseams"},{$match : {"inseams.inseam":28}},{$project : {_id : 1,
-    //             name: 1,
-    //             color : 1,
-    //             image: 1,
-    //             size : 1,
-    //             inseam:"$inseams.inseam",
-    //             price:"$inseams.price",
-    //             stock:"$inseams.stock"}},
-    //         {$match : {stock:{$gt:0}}}
-    //         ])
-            
-    //     return NextResponse.json(results || {});
-    // }
-
 }
 
 //images
-
-
-
-//cart: i need product id, quantinty,and size
