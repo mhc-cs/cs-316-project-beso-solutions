5eb9fbbf68cd2eeec4fab8ea0d22b52e5e79957b
GitxGot <rodri27b@mtholyoke.edu>
2024-04-16 16:56:23 -0400

    cart chkout: add interactions w prdct db at chkout

--- app/api/cart/checkout cart/route.ts
+++ app/api/cart/checkout cart/route.ts
@@ -1,17 +1,36 @@
 import {NextResponse} from 'next/server';
 import {ProductModel} from '../../db';
+import {CartModel} from '../../db';
 
 export async function POST(request: Request) {
     const url = new URL(request.url);
     // const res = await fetch(request)
     // const url = await res.url;
     const searchParams = new URLSearchParams(url.search);
-    
-    const cart = await ProductModel.find(
-        {category: searchParams.get('userID')}
+
+    const cart = await CartModel.findOne(
+        {userID: searchParams.get('userID')},
+        { _id: 0, items: 1,  }
     );
     
-
+    //decrease actual stock in product db by amount checked out
+    const checkoutItems = cart.items
+    for (let i = 0; i < checkoutItems.length; i++) {
+        try {
+            await ProductModel.updateOne(
+                {"name": checkoutItems[i].name, "colors.color": checkoutItems[i].color,
+                "colors.sizes.size": checkoutItems[i].size},
+                { $inc: { "colors.$[k].sizes.$[i].inseams.$[j].stock": -checkoutItems[i].quantity} },
+                { arrayFilters: [{"k.color": checkoutItems[i].color},{ "i.size": checkoutItems[i].size },
+                {"j.inseam": checkoutItems[i].inseam}] }
+            )
+        }
+        catch (err) {
+            //TODO: neg stock error handling
+           console.log(err);
+          }
+      }
+    
     //TODO: checkout?
     
     return NextResponse.json(results || {});
