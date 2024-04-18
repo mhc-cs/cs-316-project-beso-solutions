51a3ae5e380130db8d0b8290011427b99f62c383
GitxGot <rodri27b@mtholyoke.edu>
2024-04-18 15:20:37 -0400

    db: fix double export

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,23 +1,13 @@
 import mongoose from 'mongoose';
-import Image from "next/image";
-import styles from "./page.module.css";
-import { url } from "inspector";
-import React, { createFactory, useEffect, useState } from 'react';
-import Axios from 'axios';
-import Link from 'next/link';
-import MenuSize from '../components/MenuSize';
-import MenuCategory from '../components/MenuCategory';
-import MenuInseam from '../components/MenuInseam';
-import MenuColor from '../components/MenuColor';
-import Select from 'react-select'
-
+const { Schema } = mongoose;
+mongoose.Promise = global.Promise;
 // Connect to the database
-mongoose.connect(process.env.MONGODB_URI!, {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/maindb', { useNewUrlParser: true});
 // const productCategories = ['shirt', 't-shirt', 'shorts', 'jeans']
 const orderStatuses = ['not started','ordered', 'shipped', 'delivered', 'failed']
 const paymentStatuses = ['not started', 'started','processed', 'failed']
 
-const ImageSchema = new mongoose.Schema({ 
+const ImageSchema = new Schema({ 
     img: { 
        data: Buffer, 
        contentType: String 
@@ -26,7 +16,7 @@ const ImageSchema = new mongoose.Schema({
 );
 
 // Define the schema for the data
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new Schema({
         name: String,
         description: String, //client description of their product
         category: String,//shirt, t shirt, 
@@ -34,7 +24,8 @@ const ProductSchema = new mongoose.Schema({
         colors:[
             {
                 color: String, //dark wash/ medium wash
-                image: [ImageSchema],
+                // image: [ImageSchema],
+                image: String,
                 sizes:[{
                     size: String,
                     inseams:[{
@@ -57,7 +48,7 @@ const ProductSchema = new mongoose.Schema({
 });
 
 
-const CartSchema = new mongoose.Schema({
+const CartSchema = new Schema({
     userID: String,
     paymentStatus: { type: String, enum: paymentStatuses, default: 'not started'}, //client description of their product
     orderStatus: { type: String, enum: orderStatuses, default: 'not started'},//shirt, t shirt, 
@@ -71,41 +62,14 @@ const CartSchema = new mongoose.Schema({
             }]
 });
 
-// const Pants = new mongoose.Schema({
-//     category: "Pants",//shirt, t shirt, 
-//     name: String,
-//     productType:  String, //ex: ['jean', 'long', 'short', 'cargo']
-//     description: String, //client description of their product
-//     material: String, //jean, cotton, ect
-//     colors:[
-//         {
-//             color: String,
-//             sizes:[{
-//                 size: String,
-//                 inseams:[{
-//                     inseam: Number,
-//                     price: Number,
-//                     stock: Number,
-//                 }]
-//             }]
-//         }
-//     ],
-    
-// });
+// export const ImageModel = mongoose.model ('images', ImageSchema);
 
+// export const ProductModel = mongoose.model ('products', ProductSchema);
 
-// Create a model.  This is what provides the nice API to
-// manipulate the database.
-
-
-
-export const ImageModel = mongoose.model ('images', ImageSchema);
-
-export const ProductModel = mongoose.model ('products', ProductSchema);
-
-export const CartModel = mongoose.model ('cart', CartSchema);
+// export const CartModel = mongoose.model ('cart', CartSchema);
 
 // Make the model and schema available
-module.exports = ProductModel;
-module.exports = ImageModel;
-module.exports = CartModel;
+module.exports = mongoose.models.Images || mongoose.model ('Images', ImageSchema);
+module.exports = mongoose.models.ProductModel || mongoose.model('ProductModel', ProductSchema);
+module.exports = mongoose.models.Cart || mongoose.model ('Cart', CartSchema);
+
