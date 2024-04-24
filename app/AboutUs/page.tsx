0a54706d52916a9b03cce2b0728c50828a8c87fd
obrya22s <obrya22s@mtholyoke.edu>
2024-04-24 00:41:36 -0400

    products

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -20,14 +20,14 @@ export default function Page() {
 
   return (
   <div>
-    <body>
+    <body className="product-bg">
       <Topnav setActiveLink="about"/>
 
       <section>
-        <h1>About Us</h1>
+        <h1 style={{ color: 'white' }}>About Us</h1>
         <div className="row">
           <div className="column2">
-            <p>MISSI is a premier US clothing brand dedicated to catering to petite women with 
+            <p style={{ color: 'white' }}>MISSI is a premier US clothing brand dedicated to catering to petite women with 
               heights ranging from 4’11 to 5’4. Specializing in various denim styles meticulously 
               crafted to perfectly complement petite frames, our garments are designed with 
               precision and passion. We take pride in sourcing high-quality materials and 
@@ -37,7 +37,7 @@ export default function Page() {
             </p>
           </div>
           <div className="column2">
-            <img src="app/images/Missi.jpg"/>
+            <Image src="/app/images/Missi.jpg" alt="MISSI" width={500} height={500} />
           </div>
         </div>
       </section>
