7b27a8a8903977a4930bcbd8f2b516ef165b0f10
GitxGot <rodri27b@mtholyoke.edu>
2024-05-05 15:58:57 -0400

    cart: consolidate queries

---
+++ app/api/cart/route.ts
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+import {CartModel} from '../db';
+
+export async function POST(req: Request) {
+    const data = await req.json();
+    const exists = await ProductModel.find(
+        {userID: data.userID}
+    );
+    if (exists.length == 0){ //brand new cart
+        const doc = new CartModel(    {
+            _id: new mongoose.Types.ObjectId(),
+            userID: data.userID,
+            paymentStatus: data.paymentStatus, //client description of their product
+            orderStatus: data.orderStatus,//shirt, t shirt, 
+            items:[{
+                        name: data.itemName,
+                        color: data.itemColor,
+                        size: data.itemSize,
+                        inseam: data.itemInseam,
+                        quantity: data.itemQuantity,
+                        price: data.itemPrice
+                    }]
+    
+                })
+        try{
+            await doc.save();
+        }catch (error) {
+            return new Response(`Webhook error: ${error.message}`, {
+                status: 400,
+            })
+            }
+            console.log("HAPPY")
+    
+            return new Response('Success!', {
+            status: 200,
+            })
+    }else{ //update cart
+        const updateStatus = await CartModel.updateOne(
+            {"userID": data.name, 
+            "items.name": data.itemName,
+            "items.color": data.itemColor,
+            "items.size": data.itemSize,
+            "items.inseam": data.itemInseam},
+            { $inc: { "items.quantity": data.quantity} })
+    
+        if(updateStatus.modifiedCount == 0){ //no exisiting version of this product in cart
+            await CartModel.updateOne(
+                {"userID": data.userID},
+                { "$push": 
+                    {"items": 
+                        {
+                            "name": data.itemName,
+                            "color": data.itemColor,
+                            "size": data.itemSize,
+                            "inseam": data.itemInseam,
+                            "quantity": data.itemQuantity,
+                            "price": data.itemPrice
+                        }
+                    }
+                })
+            }
+    }
+  }
